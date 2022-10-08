b1586af2001767b47ba701deed236373b85d7552
sergr972 <sergr972@gmail.com>
2022-10-08 18:40:47 +0300

    VariablesTheme

--- VariablesTheme/src/VariablesTheme.java
+++ VariablesTheme/src/VariablesTheme.java
@@ -1,55 +1,54 @@
 class VariablesTheme {
-
     public static void main(String[] args) {
-        System.out.println("1. Создание переменных и вывод их значений на консоль");
+        System.out.println("1. РЎРѕР·РґР°РЅРёРµ РїРµСЂРµРјРµРЅРЅС‹С… Рё РІС‹РІРѕРґ РёС… Р·РЅР°С‡РµРЅРёР№ РЅР° РєРѕРЅСЃРѕР»СЊ");
         byte numCore = 2;
         short ram  = 8;
         int ssd = 520;
         long freqCpu = 2400;
         float weight = 2.7f;
         double diag = 15.6;
-        System.out.println("Количество ядер процессора: " + numCore + "\nЧастота процессора, Ггц: " + freqCpu
-                + "\nОбъём ОЗУ, Гб: " + ram + "\nОбъём SSD, Гб: " + ssd + "\nВес, кг: " + weight
-                + "\nДиагональ экрана: " + diag);
-        System.out.println("\n2. Расчет стоимости товара со скидкой");
+        System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ СЏРґРµСЂ РїСЂРѕС†РµСЃСЃРѕСЂР°: " + numCore + "\nР§Р°СЃС‚РѕС‚Р° РїСЂРѕС†РµСЃСЃРѕСЂР°, Р“РіС†: " + freqCpu
+                + "\nРћР±СЉС‘Рј РћР—РЈ, Р“Р±: " + ram + "\nРћР±СЉС‘Рј SSD, Р“Р±: " + ssd + "\nР’РµСЃ, РєРі: " + weight
+                + "\nР”РёР°РіРѕРЅР°Р»СЊ СЌРєСЂР°РЅР°: " + diag);
+        System.out.println("\n2. Р Р°СЃС‡РµС‚ СЃС‚РѕРёРјРѕСЃС‚Рё С‚РѕРІР°СЂР° СЃРѕ СЃРєРёРґРєРѕР№");
         int pricePen = 100;
         int priceBook = 200;
         int sum = pricePen + priceBook;
         double discount = sum * 0.11;
         double res = sum - discount;
-        System.out.println("I. Сумма скидки: " + discount);
-        System.out.println("II. Общая стоимость товара со скидкой: " + res);
-        System.out.println("\n3. Вывод на консоль слова JAVA");
+        System.out.println("I. РЎСѓРјРјР° СЃРєРёРґРєРё: " + discount);
+        System.out.println("II. РћР±С‰Р°СЏ СЃС‚РѕРёРјРѕСЃС‚СЊ С‚РѕРІР°СЂР° СЃРѕ СЃРєРёРґРєРѕР№: " + res);
+        System.out.println("\n3. Р’С‹РІРѕРґ РЅР° РєРѕРЅСЃРѕР»СЊ СЃР»РѕРІР° JAVA");
         System.out.println("    J     A    V     V    A");
         System.out.println("    J    A A    V   V    A A");
         System.out.println("J   J   AAAAA    V V    AAAAA");
         System.out.println(" J J   A     A    V    A     A");
-        System.out.println("\n4. Отображение min и max значений числовых типов данных");
+        System.out.println("\n4. РћС‚РѕР±СЂР°Р¶РµРЅРёРµ min Рё max Р·РЅР°С‡РµРЅРёР№ С‡РёСЃР»РѕРІС‹С… С‚РёРїРѕРІ РґР°РЅРЅС‹С…");
         byte maxByte = 127;
-        System.out.println("Максимальное значение byte: " + maxByte);
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ byte: " + maxByte);
         byte incByte = ++maxByte;
-        System.out.println("Значение byte после инкремента: " + incByte);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ byte РїРѕСЃР»Рµ РёРЅРєСЂРµРјРµРЅС‚Р°: " + incByte);
         byte decByte = --incByte;
-        System.out.println("Значение byte после декремента: " + decByte);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ byte РїРѕСЃР»Рµ РґРµРєСЂРµРјРµРЅС‚Р°: " + decByte);
         short maxShort  = 32767;
-        System.out.println("Максимальное значение short: " + maxShort);
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ short: " + maxShort);
         short incShort = ++maxShort;
-        System.out.println("Значение short после инкремента: " + incShort);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ short РїРѕСЃР»Рµ РёРЅРєСЂРµРјРµРЅС‚Р°: " + incShort);
         short decShort = --incShort;
-        System.out.println("Значение short после декремента: " + decShort);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ short РїРѕСЃР»Рµ РґРµРєСЂРµРјРµРЅС‚Р°: " + decShort);
         int maxInt = 2147483647;
-        System.out.println("Максимальное значение int: " + maxInt);
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ int: " + maxInt);
         int incInt = ++maxInt;
-        System.out.println("Значение int после инкремента: " + incInt);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ int РїРѕСЃР»Рµ РёРЅРєСЂРµРјРµРЅС‚Р°: " + incInt);
         int decInt = --incInt;
-        System.out.println("Значение int после декремента: " + decInt);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ int РїРѕСЃР»Рµ РґРµРєСЂРµРјРµРЅС‚Р°: " + decInt);
         long maxLong  = 9223372036854775807L;
-        System.out.println("Максимальное значение long: " + maxLong);
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ long: " + maxLong);
         long incLong = ++maxLong;
-        System.out.println("Значение long после инкремента: " + incLong);
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ long РїРѕСЃР»Рµ РёРЅРєСЂРµРјРµРЅС‚Р°: " + incLong);
         long decLong = --incLong;
-        System.out.println("Значение long после декремента: " + decLong);
-        System.out.println("\n5. Перестановка значений переменных");
+        System.out.println("Р—РЅР°С‡РµРЅРёРµ long РїРѕСЃР»Рµ РґРµРєСЂРµРјРµРЅС‚Р°: " + decLong);
+        System.out.println("\n5. РџРµСЂРµСЃС‚Р°РЅРѕРІРєР° Р·РЅР°С‡РµРЅРёР№ РїРµСЂРµРјРµРЅРЅС‹С…");
 
 
 
