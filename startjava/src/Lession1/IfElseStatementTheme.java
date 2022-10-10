c21147927f70451c8d95013ed777f6710f875f9e
sergr972 <sergr972@gmail.com>
2022-10-10 22:34:27 +0300

    Новая тема

--- startjava/src/Lession1/IfElseStatementTheme.java
+++ startjava/src/Lession1/IfElseStatementTheme.java
@@ -4,5 +4,34 @@ public class IfElseStatementTheme {
     public static void main(String[] args) {
         System.out.println("1. Перевод псевдокода на язык Java");
 
+//        if (age > 20) {
+//            System.out.println("кандидат подходит по возрасту");
+//        } else {
+//            System.out.println("не подходит");
+//        }
+//
+//        if (!men) {
+//            System.out.println("women");
+//        } else {
+//            System.out.println("men");
+//        }
+//
+//        if (height < 1.80) {
+//            System.out.println("рост ниже 180");
+//        } else {
+//            System.out.println("кандидат подходит по росту");
+//        }
+//
+//
+//        firstLetterName = "Name".charAt(0);
+//        if (firstLetterName == 'M') {
+//            System.out.println("Maxim");
+//        } else if (firstLetterName == 'I') {
+//            System.out.println("Ivan");
+//        } else {
+//            System.out.println("Oleg");
+//        }
+        System.out.println("\n2. Поиск max и min числа");
+
     }
 }
