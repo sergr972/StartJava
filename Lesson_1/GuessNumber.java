5543b10ca7ef10579dd3a5a1df2a1256fd2021cf
sergr972 <sergr972@gmail.com>
2022-10-30 18:28:23 +0300

    Угадай число

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -2,6 +2,18 @@ package Lesson_1;
 
 public class GuessNumber {
     public static void main(String[] args) {
+        int a = 21; //число игрока
+        int b = 10;
+        int c = b * 2;
+        if (a > 0 & a <= 100){
+            if ( a < c) {
+                System.out.println("число " + a + " меньщше того, что загадал компьютер");
+            } else if (a > c) {
+                System.out.println("число " + a + " больше того, что загадал компьютер");
+            } else {
+                System.out.println("Вы победили!");
+            }
+        }
 
     }
 }
