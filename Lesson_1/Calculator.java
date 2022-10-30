4367aacdbaefdb0fa6003f44aff1dd7be9457edf
sergr972 <sergr972@gmail.com>
2022-10-30 16:38:49 +0300

    Калькулятор

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,4 +1,27 @@
 package Lesson_1;
 
 public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+        int result = 1;
+        char sign = '^'; // +, -, *, /, ^, %
+        if (sign == '+') {
+            result = a + b;
+        } else if (sign == '-') {
+            result = a - b;
+        } else if (sign == '*') {
+            result = a * b;
+        } else if (sign == '/') {
+            result = a / b;
+        } else if (sign == '^') {
+            for (int i = 1; i <= b; i++) {
+                result *= a;
+            }
+        } else if (sign == '%') {
+            result = a % b;
+        }
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
 }
