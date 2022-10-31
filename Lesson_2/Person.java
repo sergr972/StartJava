7b2a112f5967914abe0b86045c2f7f192df33313
sergr972 <sergr972@gmail.com>
2022-10-31 22:12:38 +0300

    Урок 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,26 @@
+package Lesson_2;
+
+public class Person {
+
+    String genderMale = "Male";
+    String name = "Ivan";
+    double height = 1.85;
+    byte weight = 80;
+    byte age = 35;
+
+    void walk() {
+        System.out.println("walk");;
+    }
+    void sit() {
+        System.out.println("sit");
+    }
+    void run() {
+        System.out.println("run");
+    }
+    void talk() {
+        System.out.println("talk");
+    }
+    void learnJava() {
+        System.out.println("learnJava");
+    }
+}
