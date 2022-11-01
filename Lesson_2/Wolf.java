4b4e4ad0a4d2abd14e2c1b1edd5cb83e415d3442
sergr972 <sergr972@gmail.com>
2022-11-01 21:42:08 +0300

    Wolf

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -11,15 +11,19 @@ public class Wolf {
     public void walk() {
         System.out.println("Идёт");
     }
+
     public void sit() {
         System.out.println("Сидит");
     }
+
     public void run() {
         System.out.println("Бежит");
     }
+
     public void howl() {
         System.out.println("Воет");
     }
+
     public void hunt() {
         System.out.println("Охотиться");
     }
