c45e76801db32f55d738a0a9b724dfde31db94f3
sergr972 <sergr972@gmail.com>
2022-11-01 21:43:30 +0300

    Wolf

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -7,9 +7,7 @@ public class WolfTest {
         wolf.age = 3;
         wolf.name = "Вольф";
         wolf.color = "серый";
-
         System.out.println("Волк по имени " + wolf.name + ", + возраст - " + wolf.age + ", цвет - " + wolf.color);
-
         wolf.run();
         wolf.hunt();
 
