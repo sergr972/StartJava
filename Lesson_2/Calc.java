07693491ae49d4b7fadaa49d2566aa4c36ece16c
sergr972 <sergr972@gmail.com>
2022-11-09 12:16:45 +0300

    Calculator

---
+++ Lesson_2/Calc.java
@@ -0,0 +1,33 @@
+package Lesson_2;
+
+public class Calc {
+
+    public static int calc(int num1, int num2, char operation){
+        int result = 1;
+        switch (operation){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '^':
+                for (int i = 0; i < num2; i++) {
+                    result *= num1;
+                }
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, CalculatorTest.getOperation());
+        }
+        return result;
+    }
+
+
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,41 @@
+package Lesson_2;
+
+import java.util.Scanner;
+
+public class CalculatorTest {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = Calc.calc(num1,num2,operation);
+        System.out.println("Результат операции: "+result);
+    }
+
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int num;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+}
