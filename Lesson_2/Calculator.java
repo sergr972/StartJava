738b9a08732494a1908bbbc102865aa03eadf6b3
sergr972 <sergr972@gmail.com>
2022-11-11 11:32:35 +0300

    Calculator

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -6,21 +6,11 @@ public class Calculator {
     private int b;
     private char sign;
 
-//    public Calculator() {
-//        this.a = 0;
-//        this.b = 0;
-//        this.sign = ' ';
-//    }
+    public int ICalculator(int a, int b, char sign) {
 
-
-//    public Calculator() {
-//
-//        this.a = a;
-//        this.b = b;
-//        this.sign = sign;
-//    }
-
-    public static int calc(int a, int b, char sign){
+        this.a = a;
+        this.b = b;
+        this.sign = sign;
         int result = 1;
         switch (sign) {
             case '+':
@@ -40,9 +30,6 @@ public class Calculator {
                     result *= a;
                 }
                 break;
-            default:
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(a, b, CalculatorTest.getSign());
         }
         return result;
     }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,52 +3,28 @@ package Lesson_2;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        String answers;
+        do {
+            Scanner scanner = new Scanner(System.in);
 
-        class ReadClass {
-            public int readNextInt() {
-                int i = 0;
-                if (scanner.hasNextInt()) {
-                    i = scanner.nextInt();
-                } else {
-                    System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
-                }
-                return i;
-            }
-        }
+            Calculator calculator = new Calculator();
 
-        ReadClass readClass = new ReadClass();
-        int num1;
-        int num2;
-        System.out.println("Введите первое число: ");
-        num1 = readClass.readNextInt();
-        System.out.println("Введите второе число: ");
-        num2 = readClass.readNextInt();
+            System.out.println("Введите первое число: ");
+            int num1 = scanner.nextInt();
+            System.out.println("Введите знак математической операции:");
+            char operation = scanner.next().charAt(0);
+            System.out.println("Введите второе число: ");
+            int num2 = scanner.nextInt();
 
-        int result = Calculator.calc(num1, num2, getSign());
-        System.out.println("Результат операции: " + result);
-//        System.out.println("Хотите продолжить вычисления? [yes/no]");
-//        String answers = scanner.next();
-//        if (answers.equals("yes")) {
-//
-//        }
-//        if (answers.equals("No")) ;
-
-    }
-    public static char getSign() {
-
-        System.out.println("Введите знак математической операции:");
-        char operation;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();
-            operation = getSign();
-        }
-        return operation;
+            int result = calculator.ICalculator(num1, num2, operation);
+            System.out.println("Результат операции: " + result);
+            do {
+                System.out.println("Хотите продолжить вычисления? [yes/no]");
+                answers = scanner.next();
+            } while (!answers.equals("no") && !answers.equals("yes"));
+        } while (answers.equals("yes"));
     }
 }
