fe87faf1f9b0ff3e83637d4296dc5fdc250aeed2
sergr972 <sergr972@gmail.com>
2022-11-11 11:52:48 +0300

    Jaeger

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,28 @@
+package Lesson_2;
+
+public class Jaeger {
+
+    String modelName = "Bracer Phoenix";
+    String mark = "Mark-5";
+    String origin = "USA";
+    float height = 70.7f;
+    float weight = 2.1f;
+    int strehght = 8;
+    int armor = 9;
+
+    boolean drift() {
+        return true;
+    }
+
+    void  move() {
+        System.out.println("moving");
+    }
+
+    String scanKaiju() {
+        return "scanning";
+    }
+
+    void userVortexCannon() {
+        System.out.println("activated");
+    }
+}
