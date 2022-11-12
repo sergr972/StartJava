37b0333097b3629edbfc52d620738ab49195edf9
sergr972 <sergr972@gmail.com>
2022-11-12 19:19:31 +0300

    Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -2,10 +2,10 @@ package Lesson_2;
 
 public class Jaeger {
 
-        String modelName;
-        String mark;
-        float height;
-        float weight;
+        private String modelName;
+        private String mark;
+        private float height;
+        private float weight;
 
         void  move() {
                 System.out.println("шагает");
@@ -19,16 +19,32 @@ public class Jaeger {
                 this.modelName = modelName;
         }
 
+        public String getModelName() {
+                return modelName;
+        }
+
         public void setMark(String mark) {
                 this.mark = mark;
+        }
+
+        public String getMark() {
+                return mark;
         }
 
         public void setHeight(float height) {
                 this.height = height;
         }
 
+        public float getHeight() {
+                return height;
+        }
+
         public void setWeight(float weight) {
                 this.weight = weight;
+        }
+
+        public float getWeight() {
+                return weight;
         }
 
         public Jaeger() {
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -8,27 +8,26 @@ public class JaegerTest {
         jaegerOne.setMark("Mark5");
         jaegerOne.setHeight(76.2f);
         jaegerOne.setWeight(1.85f);
-        jaegerOne.move();
-        jaegerOne.scanKaiju();
 
         Jaeger jaegerTwo = new Jaeger("Bracer Phoenix", "Mark-5", 70.91f, 2.128f);
 
-        System.out.println("Имя робота - " + jaegerOne.modelName + "\nсерия - " + jaegerOne.mark +
-                "\nвысота - " + jaegerOne.height + "\nвес -" + jaegerOne.weight);
+        System.out.println("Имя робота - " + jaegerOne.getModelName() + "\nсерия - " + jaegerOne.getMark() +
+                "\nвысота - " + jaegerOne.getHeight() + "\nвес -" + jaegerOne.getWeight());
         jaegerOne.move();
         jaegerOne.scanKaiju();
 
-        System.out.println("\nИмя робота - " + jaegerTwo.modelName+ "\nсерия - " + jaegerTwo.mark +
-                "\nвысота - " + jaegerTwo.height + "\nвес -" + jaegerTwo.weight);
+        System.out.println("\nИмя робота - " + jaegerTwo.getModelName()+ "\nсерия - " + jaegerTwo.getMark() +
+                "\nвысота - " + jaegerTwo.getHeight() + "\nвес -" + jaegerTwo.getWeight());
         jaegerTwo.move();
         jaegerTwo.scanKaiju();
 
         jaegerOne.setModelName("Striker");
         jaegerOne.setMark("Mark-6");
+
         jaegerTwo = new Jaeger("Phoenix", "Mark-6", 70.91f, 2.128f);
-        System.out.println("\nИмя робота - " + jaegerOne.modelName + "\nсерия - " + jaegerOne.mark +
-                "\nвысота - " + jaegerOne.height + "\nвес -" + jaegerOne.weight);
-        System.out.println("\nИмя робота - " + jaegerTwo.modelName+ "\nсерия - " + jaegerTwo.mark +
-                "\nвысота - " + jaegerTwo.height + "\nвес -" + jaegerTwo.weight);
+        System.out.println("\nИмя робота - " + jaegerOne.getModelName() + "\nсерия - " + jaegerOne.getMark() +
+                "\nвысота - " + jaegerOne.getHeight() + "\nвес -" + jaegerOne.getWeight());
+        System.out.println("\nИмя робота - " + jaegerTwo.getModelName()+ "\nсерия - " + jaegerTwo.getMark() +
+                "\nвысота - " + jaegerTwo.getHeight() + "\nвес -" + jaegerTwo.getWeight());
     }
 }
