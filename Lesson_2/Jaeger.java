0846521e185bece3782501d9a923972c9c30dd4d
sergr972 <sergr972@gmail.com>
2022-11-13 10:44:15 +0300

    Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,11 +1,51 @@
 package Lesson_2;
 
 public class Jaeger {
-
         private String modelName;
         private String mark;
         private float height;
         private float weight;
+
+        public Jaeger() {
+        }
+
+        public Jaeger(String modelName, String mark, float height, float weight) {
+                this.modelName = modelName;
+                this.mark = mark;
+                this.height = height;
+                this.weight = weight;
+        }
+
+        public String getModelName() {
+                return modelName;
+        }
+
+        public void setModelName(String modelName) {
+                this.modelName = modelName;
+        }
+        public String getMark() {
+                return mark;
+        }
+
+        public void setMark(String mark) {
+                this.mark = mark;
+        }
+
+        public float getHeight() {
+                return height;
+        }
+
+        public void setHeight(float height) {
+                this.height = height;
+        }
+
+        public float getWeight() {
+                return weight;
+        }
+
+        public void setWeight(float weight) {
+                this.weight = weight;
+        }
 
         void  move() {
                 System.out.println("шагает");
@@ -13,47 +53,6 @@ public class Jaeger {
 
         void scanKaiju() {
                 System.out.println("сканирует");
-        }
-
-        public void setModelName(String modelName) {
-                this.modelName = modelName;
-        }
-
-        public String getModelName() {
-                return modelName;
-        }
-
-        public void setMark(String mark) {
-                this.mark = mark;
-        }
-
-        public String getMark() {
-                return mark;
-        }
-
-        public void setHeight(float height) {
-                this.height = height;
-        }
-
-        public float getHeight() {
-                return height;
-        }
-
-        public void setWeight(float weight) {
-                this.weight = weight;
-        }
-
-        public float getWeight() {
-                return weight;
-        }
-
-        public Jaeger() {
-        }
-        public Jaeger(String modelName, String mark, float height, float weight) {
-                this.modelName = modelName;
-                this.mark = mark;
-                this.height = height;
-                this.weight = weight;
         }
 }
 
