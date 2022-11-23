d3aae9220c61efb732c95f43a90bf830da0bc003
sergr972 <sergr972@gmail.com>
2022-11-23 22:23:11 +0300

    gitignore

---
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -0,0 +1,14 @@
+package com.startjava.lesson_2_3_4.array;
+
+public class ArrayTheme {
+
+    public static void main(String[] args) {
+        System.out.println("1. Реверс значений массива");
+        System.out.println("\n2. Вывод произведения элементов массива");
+        System.out.println("\n3. Удаление элементов массива");
+        System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
+        System.out.println("\n5. Генерация уникальных чисел");
+        System.out.println("\n6. Сдвиг элементов массива");
+        
+    }
+}
