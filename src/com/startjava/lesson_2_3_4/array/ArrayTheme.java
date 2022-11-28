441266879f78fcb59c77b191c209da9c34f57a5c
sergr972 <sergr972@gmail.com>
2022-11-28 21:13:32 +0300

    работа над ошибками

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -5,69 +5,63 @@ import java.util.Arrays;
 public class ArrayTheme {
     public static void main(String[] args) {
         System.out.println("1. Реверс значений массива");
-        int[] intArr;
-        int i, j;
-        intArr = new int[]{1, 3, 2, 5, 4, 7, 6};
-        int[] newArray = new int[intArr.length];
+        int[] intArr = {1, 2, 3, 4, 5, 6, 7};
+        int length = intArr.length;
         System.out.println("массив до модификации");
-        printArray1(intArr);
-        for (i = 0, j = intArr.length - 1; i < intArr.length; i++, j--) {
-            newArray[i] = intArr[j];
+        printIntArray(intArr);
+        for (int i = 0, j = length - 1; i < length / 2; i++, j--) {
+            int tmp = intArr[i];
+            intArr[i] = intArr[j];
+            intArr[j] = tmp;
         }
         System.out.println("\nмассив после модификации");
-        printArray1(newArray);
+        printIntArray(intArr);
 
         System.out.println("\n\n2. Вывод произведения элементов массива");
         intArr = new int[10];
-        for (i = 0; i < 10; i++) {
+        length = intArr.length;
+        for (int i = 0; i < length; i++) {
             intArr[i] = i;
         }
         int mult = 1;
-        for (i = 1; i < 9; i++) {
+        for (int i = 1; i < length - 1; i++) {
             mult *= intArr[i];
             System.out.print(i < 8 ? intArr[i] + " * " : intArr[i] + " = ");
         }
         System.out.println(mult);
-        for (i = 0; i < 10; i++) {
-            System.out.print(intArr[i] == 0 ? "индекс числа " + intArr[i] + " равен " + i : "");
-            System.out.print(intArr[i] == 9 ? "\nиндекс числа " + intArr[i] + " равен " + i : "");
-        }
-        //через метод
-        System.out.println("\nиндекс числа 0 равен " + Arrays.binarySearch(intArr, 0));
-        System.out.println("индекс числа 9 равен " + Arrays.binarySearch(intArr, 9));
+        System.out.print("значение массива с индексом 0 равно " + intArr[0]);
+        System.out.print("\nзначение массива с индексом 9 равно " + intArr[9]);
 
-        System.out.println("\n3. Удаление элементов массива");
+        System.out.println("\n\n3. Удаление элементов массива");
         double[] doubleArr = new double[15];
-        for (i = 0; i < doubleArr.length - 1; i++) {
+        length = doubleArr.length;
+        for (int i = 0; i < length; i++) {
             doubleArr[i] = Math.random();
         }
-        int averageIndex = doubleArr.length / 2;
-        double averageNum = doubleArr[averageIndex];
+        int averageIndex = length / 2;
+        double middleNum = doubleArr[averageIndex];
         System.out.println("индекс средней ячейки - " + averageIndex);
-        System.out.printf("%s %.3f %s", "число средней ячейки - ", averageNum, "\n\n");
+        System.out.printf("%s %.3f %s", "число средней ячейки - ", middleNum, "\n\n");
         System.out.println("исходный массив");
-        printArray2(doubleArr);
-        double[] mutableArray = new double[15];
+        printDoubleArray(doubleArr);
         int countZero = 0;
-        for (i = 0; i < doubleArr.length - 1; i++) {
-            if (doubleArr[i] > 0) {
-                mutableArray[i] = 0;
+        for (int i = 0; i < length; i++) {
+            if (doubleArr[i] > middleNum) {
+                doubleArr[i] = 0;
                 countZero++;
-            } else {
-                mutableArray[i] = doubleArr[i];
             }
         }
         System.out.println("\nизмененный массив");
-        printArray2(mutableArray);
+        printDoubleArray(doubleArr);
         System.out.println("\nколичество обнуленных ячеек - " + countZero);
 
         System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
         char[] alfabet = new char[26];
-        for (i = 0; i < 26; i++) {
+        for (int i = 0; i < 26; i++) {
             alfabet[i] = (char) (i + 65);
         }
-        for (i = alfabet.length - 1; i >= 0; i--) {
-            for (j = alfabet.length - 1; j >= i; j--) {
+        for (int i = alfabet.length - 1; i >= 0; i--) {
+            for (int j = alfabet.length - 1; j >= i; j--) {
                 System.out.print(alfabet[j]);
             }
             System.out.println();
@@ -75,8 +69,9 @@ public class ArrayTheme {
 
         System.out.println("\n\n5. Генерация уникальных чисел");
         intArr = new int[30];
-        for (i = 0; i < 30; ) {
+        for (int i = 0; i < 30; ) {
             int randomNumber = (int) (Math.random() * 40 + 60);
+            int j;
             for (j = 0; j < i; j++) {
                 if (intArr[j] == randomNumber) {
                     break;
@@ -88,8 +83,8 @@ public class ArrayTheme {
             }
         }
         Arrays.sort(intArr);
-        for (i = 0; i < intArr.length - 1; i++) {
-            for (j = 0; j < 10; j++) {
+        for (int i = 0; i < intArr.length - 1; i++) {
+            for (int j = 0; j < 10; j++) {
                 if (i < intArr.length) {
                     System.out.printf("%2d %s", intArr[i], " ");
                 }
@@ -100,13 +95,13 @@ public class ArrayTheme {
         }
     }
 
-    private static void printArray1(int[] array) {
+    private static void printIntArray(int[] array) {
         for (int numArray : array) {
             System.out.print(numArray + " ");
         }
     }
 
-    private static void printArray2(double[] array) {
+    private static void printDoubleArray(double[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < 8; j++) {
                 if (i < array.length) {
