270aac619c053bac32da5e7cccde8a56dc258d50
sergr972 <sergr972@gmail.com>
2022-12-04 13:48:22 +0300

    Модифицировал CalculatorTest + Calculator

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -6,28 +6,27 @@ public class CalculatorTest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userAnswer = "yes";
-        while (userAnswer.equals("yes")) {
-            Calculator calc = new Calculator();
-            System.out.println("Калькулятор запущен.");
-            System.out.println("Введите математическое выражение");
-            System.out.println("в формате 2 ^ 10, пробелы обязательны");
-//            String str = scanner.nextLine();
-            String str = "2 ^ 3";
-            String[] strings = str.split(" ", 3);
-            int firstNumber = Integer.parseInt(strings[0]);
-            calc.setFirstNumber(firstNumber);
-            char mathOperation = strings[1].charAt(0);
-            calc.setMathOperation(mathOperation);
-            int secondNumber = Integer.parseInt(strings[2]);
-            calc.setSecondtNumber(secondNumber);
+        String answer = "yes";
+        do {
+            if ("yes".equals(answer)) {
+                Calculator calc = new Calculator();
+                System.out.println("Калькулятор запущен.");
+                System.out.println("Введите математическое выражениев формате 2 ^ 10.");
+                System.out.print("Пробелы обязательны: ");
+            String str = scanner.nextLine();
+                String[] strings = str.split(" ", 3);
+                int firstNumber = Integer.parseInt(strings[0]);
+                calc.setFirstNumber(firstNumber);
+                char mathOperation = strings[1].charAt(0);
+                calc.setMathOperation(mathOperation);
+                int secondNumber = Integer.parseInt(strings[2]);
+                calc.setSecondtNumber(secondNumber);
 
-            calc.calculate();
-            do {
-                System.out.println("Хотите продолжить вычисления? [yes/no]");
-                userAnswer = scanner.next();
-            } while(!userAnswer.equals("no") && !userAnswer.equals("yes"));
-        }
+                calc.calculate();
+            }
+            System.out.println("Хотите продолжить вычисления? [yes/no]");
+            answer = scanner.nextLine();
+        } while (!"no".equals(answer));
         System.out.println("Работа завершена!");
     }
 }
