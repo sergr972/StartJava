ec324cf21a0e72a74386a7fc8a5e7485b681ffd1
sergr972 <sergr972@gmail.com>
2022-12-21 23:34:17 +0300

    Book начало

--- src/com/startjava/graduation/bookshelf/Book.java
+++ src/com/startjava/graduation/bookshelf/Book.java
@@ -1,4 +1,17 @@
 package com.startjava.graduation.bookshelf;
 
 public class Book {
+
+    private final String title;
+    private final String author;
+    private final int publishYear;
+    private final int LENGHT_BOOK_INFO = 3;
+
+    public Book(String title, String author, int publishYear) {
+        this.title = title;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+
+
 }
