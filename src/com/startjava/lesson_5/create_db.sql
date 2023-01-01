748bd1d4b50b532db34169bd4cd375e8e64540a6
sergr972 <sergr972@gmail.com>
2023-01-01 20:37:30 +0300

    базы данных

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,12 +1,13 @@
-CREATE DATABASE Jaegers;
+-- create database
+
 CREATE TABLE Jaegers (
-	id SERIAL PRIMARY KEY,
-	modelName TEXT,
-	mark      char(6),
-	height 
-	weight
-	status
-	origin
-	laungh
-	kaijuKill )
-;
+	id          SERIAL PRIMARY KEY,
+	modelName   TEXT,
+	mark        char(6),
+	height      NUMERIC(4, 2),
+	weight      NUMERIC(4, 3),
+	status      BOOLEAN,
+	origin      TEXT,
+    launch      DATE,
+    kaijuKill   INTEGER
+);
