4dc6b8b035a68e510617913fbf42ffe379ee060f
sergr972 <sergr972@gmail.com>
2023-01-01 20:54:46 +0300

    базы данных

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -2,13 +2,13 @@
 
 INSERT INTO Jaegers(modelName,	mark,	height,	weight,	statusActive, origin, launch, kaijuKill)
     VALUES
-        (Mark-1,	85.34,	2.412	no,	    Russia,	    2015,	            6)
-        (Mark-1,	85.34	2.312	no,	    Japan,	    December 30 2015,	2)
-        (Mark-3,	79.25,	1.980,	no,	    USA,	    July 10 2017,	    9)
-        (Mark-5,	76.20,	1,850,	no, 	Australia,	November 2 2019,	11)
-        (Mark-4,	76.20,	1,722,	no,	    China,	    August 22 2018, 	7)
-        (Mark-7,	76.20	1,722,	yes,	Indonesia,	January 20 2034,	1)
-        (Mark-6,	81,77	2,004	no,	    USA,	    January 12 2034,    1)
-        (Mark-1,	74.37	7,450	no,	    Japan,	    December 6 2015,	3)
-        (Mark-1,	77.72 	7,775	no,	    USA,	    December 15 2015,	2)
-        (Mark-1,	72,54	7,890	no,	    USA,	    December 22 2015,	2);
+        ('Mark-1', '85.34',	'2.412', 'no',  'Russia',	 '2015',	            '6')
+        ('Mark-1', '85.34',	'2.312', 'no',  'Japan',	 'December 30, 2015',	'2')
+        ('Mark-3', '79.25',	'1.980', 'no',  'USA',	     'July 10, 2017',	    '9')
+        ('Mark-5', '76.20',	'1,850', 'no',  'Australia', 'November 2, 2019',   '11')
+        ('Mark-4', '76.20',	'1,722', 'no',  'China',	 'August 22, 2018', 	'7')
+        ('Mark-7', '76.20',	'1,722', 'yes', 'Indonesia', 'January 20, 2034',    '1')
+        ('Mark-6', '81,77',	'2,004', 'no',  'USA',	     'January 12, 2034',    '1')
+        ('Mark-1', '74.37',	'7,450', 'no',	'Japan',	 'December 6, 2015',	'3')
+        ('Mark-1', '77.72', '7,775', 'no',	'USA',	     'December 15, 2015',	'2')
+        ('Mark-1', '72,54',	'7,890', 'no',	'USA',	     'December 22, 2015',	'2');
